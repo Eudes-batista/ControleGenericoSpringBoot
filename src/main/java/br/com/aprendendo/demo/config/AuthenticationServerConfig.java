cb2555b4b5e3f1265e04b7434179b08062f878d7
Eudes-Batista <eudeskyo@hotmail.com>
2019-10-26 14:18:18 -0300

    Adicionando segurança com OAuth2 e JWT

--- src/main/java/br/com/aprendendo/demo/config/AuthenticationServerConfig.java
+++ src/main/java/br/com/aprendendo/demo/config/AuthenticationServerConfig.java
@@ -9,12 +9,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthenticationServerConfig extends AuthorizationServerConfigurerAdapter{
-    
+public class AuthenticationServerConfig extends AuthorizationServerConfigurerAdapter {
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -23,7 +24,7 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
         clients.inMemory()
                 .withClient("angular")
                 .secret("@ngul@r0")
-                .scopes("read","write")
+                .scopes("read", "write")
                 .authorizedGrantTypes("password")
                 .accessTokenValiditySeconds(1800);
     }
@@ -32,13 +33,20 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(this.authenticationManager);
     }
-    
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("springBoot");
+        return accessTokenConverter;
+    }
+
     @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
-    
-    
+
 }
