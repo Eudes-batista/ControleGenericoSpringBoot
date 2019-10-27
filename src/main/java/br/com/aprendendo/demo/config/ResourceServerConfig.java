0ffe258f5bafbbc4bd5090d289f49645f2c9c1b0
Eudes-Batista <eudeskyo@hotmail.com>
2019-10-27 15:58:59 -0300

    Implementando repositorio de usuario

--- src/main/java/br/com/aprendendo/demo/config/ResourceServerConfig.java
+++ src/main/java/br/com/aprendendo/demo/config/ResourceServerConfig.java
@@ -5,24 +5,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("ROLE");
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -48,5 +50,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         manager.createUser(builder.username("admin").password("admin").roles("ROLE").build());
         return manager;
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
 
 }
---
+++ src/main/java/br/com/aprendendo/demo/security/AppUserDetailsService.java
@@ -0,0 +1,37 @@
+package br.com.aprendendo.demo.security;
+
+import br.com.aprendendo.demo.model.Usuario;
+import br.com.aprendendo.demo.repository.UsuarioRepositorio;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {        
+        Optional<Usuario> usuarioOptional = this.usuarioRepositorio.findByEmailIgnoreCase(email);
+        Usuario usuario = usuarioOptional.orElseThrow( () -> new UsernameNotFoundException("Usuário ou senha incoreta"));
+        return new User(email, usuario.getSenha(), getPermissoes(usuario)); 
+    }
+
+    private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        usuario.getPermissoes().forEach(permissao -> authorities.add(new SimpleGrantedAuthority(permissao.getDescricao().toUpperCase())));
+        return authorities;
+    }
+    
+}
